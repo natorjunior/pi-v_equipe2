4c8819f47528acf01eebe5f3dccec6c00faefd27
jorgelucas-rm <jorgelucas.rm@outlook.com>
2025-03-08 14:07:45 -0300

    (19)✨feat:Adding authentication service with JWT

--- backend/src/domain/controller/authentication_controller.py
+++ backend/src/domain/controller/authentication_controller.py
@@ -1,8 +1,12 @@
-from fastapi import APIRouter
-from pydantic import BaseModel
+from fastapi import APIRouter, Depends
+from sqlalchemy.orm import Session
+
+from app.src.domain.dto.login_data import LoginData
+from app.src.domain.service.authentication_service import AuthenticationService
+from app.src.infra.database.database import get_session
 
 router = APIRouter(prefix="/authentication")
 
 @router.post("/login")
-def login(user,password):
-    pass
+def login(login_data:LoginData, session:Session = Depends(get_session)):
+    return AuthenticationService(session).authenticate_user(login_data)
--- backend/src/infra/database/database.py
+++ backend/src/infra/database/database.py
@@ -1,12 +1,14 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, scoped_session
 
+from app.src.infra.database.base import Base
 from environments import constants
 
 connection_string = constants.DATABASE_URL
 engine = create_engine(connection_string)
 session_local = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 scoped_session = scoped_session(session_local)
+Base.metadata.create_all(engine)
 
 def get_session():
     session = None
